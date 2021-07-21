2888b601099f2bfef99d49969a07ac5d7f841be9
kingsuching <sucheen.sundaram@gmail.com>
2021-07-21 11:06:29 -0700

    Git

---
+++ R/git.R
@@ -0,0 +1,8 @@
+#' This function does the git commit and push process.
+#' @param repository, the R package
+#' @param message, commit message
+#' @export
+
+git <- function(repository = "Frost2021Package", message = "Working on it") {
+  system(paste("cd ..; cd ", repository, "; git commit -am ", message, "; git push", sep = ""))
+}
