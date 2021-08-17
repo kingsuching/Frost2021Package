aaf4b96b043bea0d80d35f9b18c9a9e0aec4fe42
kingsuching <sucheen.sundaram@gmail.com>
2021-08-17 11:03:55 -0700

    Working on it

---
+++ R/add_and_commit.R
@@ -0,0 +1,15 @@
+#' This function adds files to the git tracking list to solve the untracked files problem.
+#' @param files, the files
+#' @param repository, the repository
+#' @param message, commit message
+#' @export
+
+add_and_commit <- function(files, repository = "Frost2021Package", message = "Working on it", commit = TRUE) {
+  system(paste("cd ..; cd", repository))
+  for(i in files) {
+    system(paste("git add", i))
+  }
+  if(commit) {
+    git(repository, message)
+  }
+}
