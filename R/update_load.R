537205c9cd27cec5363942b204203c54d95ce826
kingsuching <sucheen.sundaram@gmail.com>
2021-08-17 15:01:14 -0700

    Working on it

--- R/update_load.R
+++ R/update_load.R
@@ -8,4 +8,7 @@ update_load <- function(package = "Frost2021Package") {
   path <- paste("/Users/sucheen/Documents/Cal Poly SLO/Academics/Summer 2021/", package, sep = "")
   devtools::document(path)
   roxygen2::roxygenise(path)
+  git()
+  devtools::install_github("kingsuching/Frost2021Package", force = TRUE)
+  q()
 }
