4a31e701927701e514fe26710ec04b6a42f02579
kingsuching <sucheen.sundaram@gmail.com>
2021-08-20 14:09:09 -0700

    function

---
+++ R/add_to_package.R
@@ -0,0 +1,12 @@
+#' This function adds code to the project R package.
+#' @import rvest
+#' @import stringr
+#' @import devtools
+#' @param file, the file to be added
+#' @export
+
+add_to_package <- function(file) {
+  Frost2021Package::copy(file)
+  system(paste("cd ..; cd Frost2021Package; git add", file))
+  Frost2021Package::git(message = paste("Added", file))
+}
