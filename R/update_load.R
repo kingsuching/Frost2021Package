cbd0eaa35614794afe5a66d90f72ce6ec3c8baef
kingsuching <sucheen.sundaram@gmail.com>
2021-08-22 13:24:38 -0700

    Working on it

--- R/update_load.R
+++ R/update_load.R
@@ -4,11 +4,13 @@
 #' @import roxygen2
 #' @export
 
-update_load <- function(package = "Frost2021Package") {
+update_load <- function(package = "Frost2021Package", install = FALSE) {
   path <- paste("/Users/sucheen/Documents/Cal Poly SLO/Academics/Summer 2021/", package, sep = "")
   devtools::document(path)
   roxygen2::roxygenise(path)
-  git()
-  devtools::install_github("kingsuching/Frost2021Package", force = TRUE)
-  q()
+  git(package = package, message = "Namespace")
+  if(install) {
+    devtools::install_github("kingsuching/Frost2021Package", force = TRUE)
+    q()
+  }
 }
