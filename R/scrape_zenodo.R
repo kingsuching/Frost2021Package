272a2744a0ee20cf660f19c503a7f0f8e54278fb
kingsuching <sucheen.sundaram@gmail.com>
2021-08-30 11:38:17 -0700

    Working on it

--- R/scrape_zenodo.R
+++ R/scrape_zenodo.R
@@ -59,9 +59,10 @@ scrape_zenodo <- function(url) {
       df$`Cite as` <- c(NA)
     }
   }
-  df <- mutate(df, FileInfo = file_info)
-  file_info <- unnest(file_info, cols = c(data))
-  df <- mutate(df, FileName = paste(file_info$Name, collapse = ", "), SizeMB = paste(file_info$SizeMB, collapse = ", "))
+  file_info <- unnest(file_info)
+  #df <- dplyr::mutate(df, FileName = paste(file_info$Name, collapse = ", "), SizeMB = paste(file_info$SizeMB, collapse = ", "))
+  df$FileName <- paste(file_info$Name, collapse = ", ")
+  df$SizeMB <- paste(file_info$SizeMB, collapse = ", ")
   df <- df[, c("Name", "Authors", "Unique views", "Unique downloads", "Publication date:", "Keyword(s):", "Communities:", "FileName", "SizeMB")]
   return(df)
 }
